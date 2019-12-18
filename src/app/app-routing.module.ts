3fc35cb729018e0b1d516882430e6773fc25e9ad
kjuubcrf <a.televca8@gmail.com>
2019-12-18 17:45:20 +0200

    added guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signin', component: LoginComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { apiProvider } from './interceptors/api.interceptor';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { LoginComponent } from './components/login/login.component';
     AppRoutingModule
   ],
   providers: [
+    AuthGuard,
     apiProvider,
   ],
   bootstrap: [AppComponent]
