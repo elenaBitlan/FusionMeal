9670a7e0718c87fd7d7b471c2865ecdfff8cd6e7
elenaBitlan <lena.bitlan@gmail.com>
2019-12-19 14:13:04 +0200

    carousel vr.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component:CarouselComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { apiProvider } from './interceptors/api.interceptor';
 import { LoginComponent } from './components/login/login.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CarouselComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
+    CarouselModule.forRoot(),
+    
    
     
   ],
   providers: [
     apiProvider,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,]
 })
 export class AppModule { }
